0e1719cc6999b6a012e695fde9ef9a26918c332c
SamoeiLinda <you@example.com>
2022-06-05 00:56:11 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-       char ch = 'a';
+char ch = 'a';
 
-       while (ch <= 'z')
-       {
-       if (ch != 'e' && ch != 'q')
+while (ch <= 'z')
+{
+if (ch != 'e' && ch != 'q')
 	       {
 		      putchar(ch);
 		}
 		ch++;
-	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
